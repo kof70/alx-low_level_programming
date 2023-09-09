318876ebadf9ba3c3cf054683c290f4cd08d9926
kof70 <djakpakoffi7029@gmail.com>
2023-09-08 18:14:08 -0700

    j ai rien compris a la tache 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char *message = "\"Programming is like building a multilingual puzzle";
 	puts(message);
+
 	return (0);
 }
