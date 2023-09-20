d756d5694d6d6462d22a81f485039308cdff5a16
kof70 <djakpakoffi7029@gmail.com>
2023-09-20 11:17:04 -0700

    if;wq else; else if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -23,6 +25,5 @@ int main(void)
 	{
 		printf("is negative\n");
 	}
-	/* your code goes there */
 	return (0);
 }
