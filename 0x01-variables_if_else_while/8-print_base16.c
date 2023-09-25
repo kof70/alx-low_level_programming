f350ea5ea03aa9f2bea146e7b32376320c4bba80
kof70 <djakpakoffi7029@gmail.com>
2023-09-25 12:31:30 -0700

    hexadecimal ....les lettres apres les chiffres

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hexa;
+
+	for (hexa = '0'; hexa <= '9'; hexa++)
+	{
+		putchar(hexa);
+	}
+
+	for (hexa = 'a'; hexa <= 'f'; hexa++)
+	{
+		putchar(hexa);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
