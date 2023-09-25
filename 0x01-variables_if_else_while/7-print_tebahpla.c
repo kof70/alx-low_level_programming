42085c2b98848a6841d2deda24bcc46920be03e1
kof70 <djakpakoffi7029@gmail.com>
2023-09-25 12:13:04 -0700

    l incremmentation (--)..

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 'z'; num >= 'a'; num--)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
