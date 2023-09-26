3e7aa4acd97f50b3fbbe66cc4e8093ce0e2ac898
kof70 <djakpakoffi7029@gmail.com>
2023-09-25 19:13:57 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num1, num2;
+
+    for (num1 = 0; num1 <= 8; num1++) /* Change the limit of num1*/
+    {
+        for (num2 = num1 + 1; num2 <= 9; num2++) /* Start num2 from num1 + 1*/
+        {
+            putchar(num1 + '0');
+            putchar(num2 + '0');
+            
+            if (num1 == 8 && num2 == 9)
+                putchar('\n');
+            else
+                putchar(',');
+        }
+    }
+
+    return (0);
+}
