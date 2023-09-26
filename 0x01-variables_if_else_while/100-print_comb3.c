f4aea83d9a65deb0f051b4cdf7e5507f99c5f7b3
kof70 <djakpakoffi7029@gmail.com>
2023-09-25 19:28:14 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,21 +7,22 @@
  */
 int main(void)
 {
-    int num1, num2;
+	int num1, num2;
 
-    for (num1 = 0; num1 <= 8; num1++) /* Change the limit of num1*/
-    {
-        for (num2 = num1 + 1; num2 <= 9; num2++) /* Start num2 from num1 + 1*/
-        {
-            putchar(num1 + '0');
-            putchar(num2 + '0');
-            
-            if (num1 == 8 && num2 == 9)
-                putchar('\n');
-            else
-                putchar(',');
-        }
-    }
+	for (num1 = 0; num1 <= 8; num1++) /* Change the limit of num1*/
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++) /* Start num2 from num1 + 1*/
+		{
+			putchar(num1 + '0');
+			putchar(' ');
+			putchar(num2 + '0');
 
-    return (0);
+			if (num1 == 8 && num2 == 9)
+				putchar('\n');
+			else
+				putchar(',');
+		}
+	}
+
+	return (0);
 }
