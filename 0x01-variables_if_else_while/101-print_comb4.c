d4f793f49ee313e5cec1de1a1b4f7ab40c52c299
kof70 <djakpakoffi7029@gmail.com>
2023-10-16 20:26:55 +0000

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-        int i, j, k;
+	int i, j, k;
 
-        for (i= 0; i < 8; i++)
-        {
-                for (j = i + 1; j < 9; j++)
-                {
-                        for (k = j + 1 ; k < 10; k++)
-                        {
-                                putchar(i + '0');
-                                putchar(j + '0');
-                                putchar(k + '0');
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1 ; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
-				if (i  < 7 || j < 8 || k < 9 )
+				if (i  < 7 || j < 8 || k < 9)
 				{
 					putchar(',');
 					putchar(' ');
@@ -26,6 +26,6 @@ int main(void)
 			}
 		}
 	}
-
+	putchar('\n');
 	return (0);
 }
