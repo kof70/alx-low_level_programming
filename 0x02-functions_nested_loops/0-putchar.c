6fe369064942de093c29d1f3b50c1a043fb802ac
kof70 <djakpakoffi7029@gmail.com>
2023-10-19 15:30:29 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char putchar[] = "_putchar ";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+
+	return (0);
+}
